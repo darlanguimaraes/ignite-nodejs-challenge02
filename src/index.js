4c0cfd8204390dfead518824636d65948966f645
Darlan Guimaraes <darlanibira@gmail.com>
2021-11-08 19:20:52 -0300

    reolucao do desafio, faltando resolver dois testes

--- src/index.js
+++ src/index.js
@@ -10,19 +10,58 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  if (!username) {
+    return response.status(404).json({error:"Error"});
+  }
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) return response.status(404).json({error:"Error"});
+  request.user = user;
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const user = request.user;
+
+  if (user.pro === false && user.todos.length < 10) {
+    next();
+    return;
+  }
+
+  return response.status(403).json({error:"Error"});
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  if (!username) {
+    return response.status(404).json({error:"Error"});
+  }
+
+  const user = users.find((user) => user.username === username);
+  if (!user) return response.status(400).json({error:"Error"});
+
+  const { id } = request.params;
+  if (!validate(id)) return response.status(400).json({ error: "error"});
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) return response.status(404).json({ error: "error"});
+
+  request.user = user;
+  request.todo = todo;
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+  if (!user) return response.status(404).json({error:"Error"});
+
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
